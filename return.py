9e2ee178bf4a518858d20f9f578718674243dd5a
0023jas <sandersonjack02@gmail.com>
2021-06-25 15:39:17 +0100

    Adding python file

---
+++ return.py
@@ -0,0 +1,59 @@
+# The year all of the data originates from
+startYear = 1871
+
+# Data for the sp500 price since 1871
+yearlyPrice = [4.44, 4.86, 5.11, 4.66, 4.54, 4.46, 3.55, 3.25, 3.58, 5.11, 6.19, 5.92, 5.81, 5.18, 4.24, 5.2, 5.58, 5.31, 5.24, 5.38, 4.84, 5.51, 5.61, 4.32, 4.25, 4.27, 4.22, 4.88, 6.08, 6.1, 7.07, 8.12, 8.46, 6.68, 8.43, 9.87, 9.56, 6.85, 9.06, 10.08, 9.27, 9.12, 9.3, 8.37, 7.48, 9.33, 9.57, 7.21, 7.85, 8.83, 7.11, 7.3, 8.9, 8.83, 10.58, 12.65, 13.4, 17.53, 24.86, 21.71, 15.98, 8.3, 7.09, 10.54, 9.26, 13.76, 17.59, 11.31, 12.5, 12.3, 10.55, 8.93, 10.09, 11.85, 13.49, 18.02, 15.21, 14.83, 15.36, 16.88, 21.21, 24.19, 26.18, 25.46, 35.6, 44.15, 45.43, 41.12, 55.62, 58.03, 59.72, 69.07, 65.06, 76.45, 86.12, 93.32, 84.45, 95.04, 102.0, 90.31, 93.49, 103.3, 118.4, 96.11, 72.56, 96.86, 103.8, 90.25, 99.71, 110.9, 133.0, 117.3, 144.3, 166.4, 171.6, 208.2, 264.5, 250.5, 285.4, 339.97, 325.49, 416.08, 435.23, 472.99, 465.25, 614.42, 766.22, 963.36, 1248.77, 1425.59, 1335.63, 1140.21, 895.84, 1132.52, 1181.41, 1278.73, 1424.16, 1378.76, 865.58, 1123.58, 1282.62, 1300.58, 1480.4, 1822.36, 2028.18, 1918.6, 2275.12, 2789.8, 2607.39, 3278.2, 3793.75]
+# Data for the sp500 yields since 1871
+yields = [0.055, 0.059, 0.075, 0.073, 0.069, 0.084, 0.058, 0.052, 0.041, 0.045, 0.053, 0.055, 0.062, 0.071, 0.046, 0.039, 0.047, 0.045, 0.041, 0.048, 0.041, 0.044, 0.057, 0.049, 0.044, 0.043, 0.038, 0.035, 0.035, 0.044, 0.04, 0.041, 0.053, 0.038, 0.035, 0.041, 0.067, 0.044, 0.043, 0.052, 0.052, 0.051, 0.06, 0.057, 0.045, 0.057, 0.102, 0.072, 0.059, 0.075, 0.063, 0.058, 0.062, 0.054, 0.048, 0.051, 0.044, 0.037, 0.045, 0.063, 0.097, 0.073, 0.044, 0.049, 0.036, 0.042, 0.073, 0.04, 0.05, 0.064, 0.081, 0.062, 0.053, 0.049, 0.038, 0.047, 0.056, 0.061, 0.069, 0.074, 0.06, 0.054, 0.058, 0.044, 0.036, 0.037, 0.044, 0.033, 0.031, 0.034, 0.028, 0.034, 0.031, 0.03, 0.03, 0.035, 0.031, 0.029, 0.035, 0.035, 0.031, 0.027, 0.036, 0.054, 0.042, 0.039, 0.05, 0.053, 0.052, 0.046, 0.054, 0.049, 0.043, 0.046, 0.038, 0.033, 0.037, 0.035, 0.032, 0.037, 0.031, 0.028, 0.027, 0.029, 0.022, 0.02, 0.016, 0.014, 0.012, 0.012, 0.014, 0.018, 0.016, 0.016, 0.018, 0.018, 0.019, 0.032, 0.02, 0.018, 0.021, 0.022, 0.019, 0.019, 0.021, 0.02, 0.018, 0.021, 0.018, 0.016, 0.014]
+# Data for yearly inflation since 1871
+yearlyInflation = [-0.07, 0, -0.02, -0.05, -0.04, -0.03, -0.02, -0.05, 0, 0.02, 0, 0, -0.01, -0.03, -0.01, -0.03, 0.01, 0, -0.03, -0.01, 0, 0, -0.01, -0.04, -0.02, 0, -0.01, 0, 0, 0.01, 0.01, 0.01, 0.02, 0.01, -0.01, 0.02, 0.04, -0.02, -0.01, 0.04, 0, 0.02, 0.02, 0.01, 0.01, 0.08, 0.17, 0.18, 0.15, 0.16, -0.11, -0.06, 0.02, 0, 0.02, 0.01, -0.02, -0.02, 0, -0.02, -0.09, -0.1, -0.05, 0.03, 0.02, 0.01, 0.04, -0.02, -0.01, 0.01, 0.05, 0.11, 0.06, 0.02, 0.02, 0.08, 0.14, 0.08, -0.01, 0.01, 0.08, 0.02, 0.01, 0.01, 0, 0.01, 0.03, 0.03, 0.01, 0.02, 0.01, 0.01, 0.01, 0.01,0.02,0.03,0.03,0.04,0.05,0.06,0.04,0.03,0.06,0.11,0.09,0.06,0.07,0.08,0.11,0.13,0.1,0.06,0.03,0.04,0.04,0.02,0.04,0.04,0.05,0.05,0.04,0.03,0.03,0.03,0.03,0.03,0.02,0.02,0.02,0.03,0.03,0.02,0.02,0.03,0.03,0.03,0.03,0.04,0,0.02,0.03,0.02,0.01,0.02,0,0.01,0.02,0.02,0.02,0.01, 0.04]
+
+# Original starting amount in 1871 money
+startingAmount = 30000
+
+# Withdrawal Sums
+percentage = 0.0331 # Percentage taken out of inflation adjusted original sum every year
+
+# Inflation Rate used to adjust percentage withdrawn
+inflationRate = 0.014
+
+# List of years in which the withdraw percentage was to large
+negativeYears = []
+
+# Nested for loop, first loop sets the start year, second loop runs simulation to end year
+for start in range(len(yearlyPrice)):
+    # Start amount grows with inflation depending on year
+    inflationStartingAmount = startingAmount*(1 + inflationRate)**(start)
+    # Starting shares are dependent on price of start
+    startingShares = inflationStartingAmount/yearlyPrice[start]
+    # Sum withdrawn grows with inflation as well
+    sumWithdrawn = inflationStartingAmount * percentage
+    # Current shares is what is used in next loop
+    currentShares = startingShares
+
+    # Loop starts at beginning of nth year
+    for year in range(start, len(yearlyPrice)):
+        # Calculating the amount needed to be withdrawn that year
+        sumWithdrawn = sumWithdrawn + sumWithdrawn*yearlyInflation[year]
+        # Calculating shares to be withdrawn
+        withdrawnShares = (sumWithdrawn / yearlyPrice[year])
+        # Calculating remaining shares
+        currentShares = currentShares - withdrawnShares
+        # Adding sp500 yield
+        currentShares = currentShares + (currentShares * yields[year])
+        # Calculating current year
+        currentYear = year + startYear
+
+    # Printing the results
+    print("")
+    print("Start Year: " + str(start + startYear))
+    print("Starting Amount: " + str(round(inflationStartingAmount,2)))
+    print("Remaining Shares: " + str(round(currentShares,2)))
+    print("Total Value: " + str(round(currentShares*yearlyPrice[-1],2)))
+
+    # Finding years in which the end result was negative
+    if(round(currentShares*yearlyPrice[-1],2) < 0):
+        negativeYears.append([start + startYear, round(currentShares*yearlyPrice[-1],2)])
+
+# Printing all negative years
+print(negativeYears)
